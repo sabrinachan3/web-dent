de01b42a84524217e4630a7ca7e52a43bcb0ff8e
Sabrina Chan <sabrina.chan3@mail.mcgill.ca>
2020-12-27 17:23:28 -0500

    working website

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import profile_pic from './Images/profile_pic.JPG'
 import MyIcon from './Components/MyIcon.js'
+import Nav from 'react-bootstrap/Nav';
 import './App.css';
 
 function App() {
@@ -9,6 +10,19 @@ function App() {
       <img className="profile_pic" src={profile_pic} alt=""></img>
       <p className="description">I am building my first website!!!</p>
       <MyIcon></MyIcon>
+      <div className= "navigation">
+        <Nav variant="tabs" defaultActiveKey="/home">
+        <Nav.Item>
+          <Nav.Link href="/home">tab 1</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-1">tab 2</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="disabled" disabled> Disabled</Nav.Link>
+        </Nav.Item>
+      </Nav></div>
+
     </div>
   );
 }

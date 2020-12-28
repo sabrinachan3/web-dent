ca798c428ce9b0e9531a722324fccdbefe436faa
Sabrina Chan <sabrina.chan3@mail.mcgill.ca>
2020-12-27 19:42:39 -0500

    iteration of website

--- src/App.js
+++ src/App.js
@@ -22,6 +22,22 @@ function App() {
           <Nav.Link eventKey="disabled" disabled> Disabled</Nav.Link>
         </Nav.Item>
       </Nav></div>
+      <Nav className="justify-content-center" activeKey="/home">
+    <Nav.Item>
+      <Nav.Link href="/home">Active</Nav.Link>
+    </Nav.Item>  <Nav.Item>
+      <Nav.Link eventKey="link-1">Link</Nav.Link>
+    </Nav.Item>
+   
+    <Nav.Item>
+      <Nav.Link eventKey="link-2">Link</Nav.Link>
+    </Nav.Item>
+    <Nav.Item>
+      <Nav.Link eventKey="disabled" disabled>
+        Disabled
+      </Nav.Link>
+    </Nav.Item>
+  </Nav>
 
     </div>
   );

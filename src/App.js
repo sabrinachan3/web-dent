43765b7a11041db1d2e19290b00895e33851bbb8
Sabrina Chan <sabrina.chan3@mail.mcgill.ca>
2020-12-27 21:18:59 -0500

    first iteration

--- src/App.js
+++ src/App.js
@@ -1,28 +1,51 @@
-import profile_pic from './Images/profile_pic.JPG'
+import evangelia_face from './Images/evangelia_face.jpg'
 import MyIcon from './Components/MyIcon.js'
-import Nav from 'react-bootstrap/Nav';
+import MyMap from './Components/MyMap.js'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="title">Sabrina Chan</header>
-      <img className="profile_pic" src={profile_pic} alt=""></img>
-      <p className="description">I am building my first website!!!</p>
-      <MyIcon></MyIcon>
-      <div className= "navigation">
-        <Nav variant="tabs" defaultActiveKey="/home">
-        <Nav.Item>
-          <Nav.Link href="/home">tab 1</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="link-1">tab 2</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="disabled" disabled> Disabled</Nav.Link>
-        </Nav.Item>
-      </Nav></div>
+    <div className="Container">
+      <div className="header">
+        <h2 className="top-bar-phone">Call now: 450-688-2522</h2>
+        <h1 className="title">Centre Dentaire Familial Valavanis</h1>
+      </div>  {/* header */}
+      
+  
+        <img className="profile_pic" src={evangelia_face} alt=""></img>
+        <p className="description">Dental clinic located in Laval</p>
+        <MyIcon></MyIcon>
 
+        <div className="opening_hours">
+          <h3 className="opening_hours_header">Opening Hours:</h3>
+          <table className="table">
+          <tr>
+            <td>Monday:</td>
+            <td>8:00 - 19:00</td> 
+          </tr>
+          <tr>
+            <td>Tuesday:</td>
+            <td>10:00 - 14:00</td> 
+          </tr>
+          <tr>
+            <td>Wednesday:</td>
+            <td>8:00 - 19:00</td> 
+          </tr>
+          <tr>
+            <td>Thursday:</td>
+            <td>8:00 - 14:00</td> 
+          </tr>
+          <tr>
+            <td>Friday:</td>
+            <td>8:00 - 14:00</td> 
+          </tr>
+          <tr>
+            <td>Saturday-Sunday:</td>
+            <td>Closed</td> 
+          </tr>
+          </table>
+        </div>
+      <MyMap></MyMap>
     </div>
   );
 }
